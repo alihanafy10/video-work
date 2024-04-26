4d3e697f9065a55b237c7712f3a9eb771e5da9a0
engali1030 <engali1030@gmail.com>
2024-04-26 19:31:03 +0300

    commit-1

---
+++ js/video.js
@@ -0,0 +1,45 @@
+
+  const lis = document.querySelectorAll(".under-div ul li");
+  const contents = document.querySelectorAll(".content");
+
+contents.forEach((el, idx) => {
+    if (idx == 1 || idx == 2) {
+          el.style.display ='none'
+      }
+  })
+
+  lis.forEach((li, index) => {
+      li.addEventListener("click", () => {
+          lis.forEach((el) => {
+            el.classList.remove("active");
+        })
+      contents.forEach((content) => {
+        content.style.display = "none";
+      });
+      li.classList.add("active");
+      contents[index].style.display = "block";
+    });
+  });
+
+
+/******************* vid ********************** */
+  
+
+
+    const playerr = new Plyr('#playerr', {
+        controls: [
+            'play-large',
+            'progress',
+            'current-time',
+            'captions',
+            
+            'fullscreen',
+        ],
+        youtube: {
+            noCookie: true,
+            rel: 0,
+            showinfo: 0,
+            iv_load_policy: 3
+        }
+    })
+
